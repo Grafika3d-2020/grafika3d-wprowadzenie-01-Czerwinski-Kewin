b529be0bd4c11d5a2c9101a985b9891f10913298
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2021-03-19 15:53:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,50 @@
-console.log(THREE)
+let scene, camera, renderer;
+function init() {
+    scene = new THREE.Scene();
 
-const renderer = new THREE.WebGLRenderer();
-const scene = new THREE.Scene();
-renderer.setSize( window.innerWidth, window.innerHeight);
-document.body.appendChild( renderer.domElement );
+    camera = new THREE.PerspectiveCamera(55,window.innerWidth/window.innerHeight,45,30000);
+    camera.position.set(-900,-200,-900);
 
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
+    renderer = new THREE.WebGLRenderer({antialias:true});
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-let controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.addEventListener("change", renderer);
+    let controls = new THREE.OrbitControls(camera,renderer.domElement);
+    controls.update();
+    controls.minDistance = 500;
+    controls.maxDistance = 1500;
 
-const LineMaterial = new THREE.LineBasicMaterial( { color: 0x80ffff } );
-const points = [];
-points.push( new THREE.Vector3( -10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 0, -10, 0 ) );
+    let materialArray = []
 
-// for (let i=1; i<5; i++)
-// {
-//     points.push( new THREE.Vector3( 5*i, -10, 0 ) );
-// }
+    let texture_ft = new THREE.TextureLoader().load('desertdawn_ft.jpg');
+    let texture_bk = new THREE.TextureLoader().load('desertdawn_bk.jpg');
+    let texture_up = new THREE.TextureLoader().load('desertdawn_up.jpg');
+    let texture_dn = new THREE.TextureLoader().load('desertdawn_dn.jpg');
+    let texture_rt = new THREE.TextureLoader().load('desertdawn_rt.jpg');
+    let texture_lf = new THREE.TextureLoader().load('desertdawn_lf.jpg');
 
-const lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
-const line = new THREE.Line( lineGeometry, LineMaterial );
-scene.add( line );
-//renderer.render( scene, camera );
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_ft}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_bk}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_up}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_dn}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_rt}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_lf}));
 
-const light1 = new THREE.PointLight({color: 0x0fff00})
-scene.add( light1 );
-light1.position.x = 50;
-light1.position.y = -20;
-light1.position.z = 10;
+    console.log(materialArray);
 
-const light2 = new THREE.PointLight({color: 0x0fff00})
-scene.add( light2 );
-light1.position.set(-50,50,20);
+    for(let i=0; i<6; i++){
+        materialArray[i].side = THREE.BackSide;
+    }
 
-//FOG
-const near = 80;
-const far = 150;
-const color = 'lightblue';
-scene.fog = new THREE.Fog(color, near, far);
-scene.background = new THREE.Color(color);
-
-const colorWhite = new THREE.Color('hsl(106, 100%, 90%)')
-
-const width = 20;
-const height = 20;
-const depth = 20;
-
-//kostka
-const cubeGeometry = new THREE.BoxGeometry( width, height, depth );
-const cubeMaterial = new THREE.MeshPhongMaterial({
-    color: colorWhite,
-    shininess: 80
-})
-
-const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
-scene.add( cube );
-
-cube.rotation.z = 0;
-cube.rotation.x = 10;
-
-cube.position.x = 20;
-cube.position.y = 0;
-cube.position.z = 0;
-//renderer.render( scene, camera );
-
-const animate = () => {
-    requestAnimationFrame(animate)
-    cube.rotation.x += .04
-    cube.rotation.y += .04
-    cube.rotation.z += .04
-    renderer.render( scene, camera );
+    let skyboxGeo = new THREE.BoxGeometry( 10000, 10000, 10000);
+    let skybox = new THREE.Mesh( skyboxGeo, materialArray);
+    scene.add( skybox );
+    animate();
 }
 
-//stozek
-const coneGeometry = new THREE.ConeGeometry( 5, 20, 32 );
-const coneMaterial = new THREE.MeshPhongMaterial( {color: 0xffff00} );
-const cone = new THREE.Mesh( coneGeometry, coneMaterial );
-scene.add( cone );
+function animate() {
+    renderer.render(scene,camera);
+    requestAnimationFrame(animate);
+}
 
-cone.position.x = -20;
-cone.position.y = 0;
-cone.position.z = 0;
-
-//cylinder
-const cylinderGeometry = new THREE.CylinderGeometry( 5, 5, 20, 20 );
-const cylinderMaterial = new THREE.MeshPhongMaterial( {color: 0xff0000} );      //basic - bez krawedzi
-const cylinder = new THREE.Mesh( cylinderGeometry, cylinderMaterial );
-scene.add( cylinder );
-
-cylinder.position.x = 0;
-cylinder.position.y = 0;
-cylinder.position.z = 0;
-
-animate()
+init();
